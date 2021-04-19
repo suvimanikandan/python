8d68038f63feecfaa492254daf08a51807aa718c
swetha_suvi <80690771+suvimanikandan@users.noreply.github.com>
2021-04-19 11:05:51 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,9 @@
+# This print out a string
+#This is a multiple line comments
+
+#print("comments are fun")
+
+''' swetha
+thangamani
+thangamalar
+this is a multiline comment'''
---
+++ functions.py
@@ -0,0 +1,6 @@
+def say_hi(name,age):
+    print(" hello " + name +" , you are " +str(age))
+
+
+say_hi(" swetha ",  20 )
+say_hi(" thangamani ", 19 )
